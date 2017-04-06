7a72ac6668b2d04aecbd7ea3abece7f434c830d8
Tyler Shambora <tyler@bvaccel.com>
2017-04-06 13:41:58 -0700

    wip

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const routes = require('./routes.js');
 app.set('port', process.env.PORT || 5000);
 
 app.use(router);
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(methodOverride());
 
 router.all('*', function(req, res, next){

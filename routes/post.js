1022efa58542f2d860f470046e206cf648d70d6a
Tyler Shambora <tyler@bvaccel.com>
2017-04-07 17:09:36 -0700

    wip

--- routes/post.js
+++ routes/post.js
@@ -1,6 +1,8 @@
 'use strict';
 
-const casperTests = require('../casper.js');
+const exec = require('child_process').exec;
+
+const sites = require('../sites/sites.js');
 
 module.exports = function (req, res) {
   console.log('Incoming Post Deploy Webhook Request');
@@ -15,7 +17,12 @@ module.exports = function (req, res) {
     let data = JSON.parse(requestData);
     console.log(data);
 
-    casperTests.run(data.repository);
+    exec(`casperjs test ./tests/casper.js ${sites[data.repository].url}`, function (error, stdout, stderr) {
+      console.log(error);
+      console.log(stdout);
+      console.log(stderr);
+    });
+
     res.status(200);
   });
 }

c16681310db5562602bb6874273c73fc879ca934
Tyler Shambora <tyler@bvaccel.com>
2017-04-08 10:23:26 -0700

    wip

--- tests/googletesting.js
+++ tests/googletesting.js
@@ -1,6 +1,6 @@
 casper.test.begin('google search retrieves 10 or more results', 5, function suite(test) {
     casper.start("http://www.google.fr/", function() {
-        test.assertTitle("google", "google homepage title is the one expected");
+        test.assertTitle("Google", "google homepage title is the one expected");
         test.assertExists('form[action="/search"]', "main form is found");
         this.fill('form[action="/search"]', {
             q: "casperjs"

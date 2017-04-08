a2fcae2bb6a97eac18fec71cfa55145961c3b0df
Tyler Shambora <tyler@bvaccel.com>
2017-04-08 10:11:47 -0700

    wip

--- tests/casper.js
+++ tests/casper.js
@@ -1,5 +1,7 @@
 casper.test.begin('cart and search icons exist', 2, function suite(test) {
-  casper.start(casper.cli.options.site, function () {
+  casper.start(casper.cli.options.site);
+
+  casper.then(function () {
     test.assertExists('.icon-cart', 'cart icon exists');
     test.assertExists('.icon-search', 'search icon exists');
   });

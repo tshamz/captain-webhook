eeb8c36c8caa92d66631b2df3c3cb4a83abbff53
Tyler Shambora <tyler@bvaccel.com>
2017-04-11 09:34:05 -0700

    adds testingThemeId and slackWebhook to grenco site data

--- sites/data/grenco.js
+++ sites/data/grenco.js
@@ -2,5 +2,7 @@
 
 module.exports = {
   repo: 'staging-gpen',
-  url: 'https://grencoscience.myshopify.com'
+  url: 'https://grencoscience.myshopify.com',
+  testingThemeId: '179083601',
+  slackWebhook: 'https://hooks.slack.com/services/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'
 };
